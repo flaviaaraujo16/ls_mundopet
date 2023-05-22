93252fe45684317e2f92e0c576ec9f8d52fdad0f
flaviaaraujo16 <104841344+flaviaaraujo16@users.noreply.github.com>
2023-05-22 09:45:52 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylels.css">
+    <title>labsoft</title>
+</head>
+<body>
+   <h1>MUNDO PET</h1> 
+   <div>
+   <img src = "https://i.ibb.co/4syrkH9/580b57fbd9996e24bc43bb93.png"></img>
+</div>
+   <h2>Bem vindo(a), Vitória!</h2>
+   <button> Configurações de administrador</button> 
+   <br>
+   <button> Configurações de secretário</button> 
+   <br>
+   <button> Sou um cliente</button> 
+   <br>
+   <button> Volte para o início</button> 
+</body>
+</html>
